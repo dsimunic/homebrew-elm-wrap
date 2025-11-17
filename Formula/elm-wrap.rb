7c7918cea9113741e9bc026431a1d78ceb69fb3d
Damir Simunic <damir@oomm.dev>
2025-11-17 11:12:34 +0100

    Update to release v1.0.1

--- Formula/elm-wrap.rb
+++ Formula/elm-wrap.rb
@@ -1,16 +1,16 @@
 class ElmWrap < Formula
   desc "Elm package management wrapper with custom registry support"
   homepage "https://github.com/dsimunic/elm-wrap"
-  version "1.0.0"
+  version "1.0.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/dsimunic/elm-wrap/releases/download/v1.0.0/elm-wrap-macos-arm64"
-      sha256 "6495b31b964a8561f4fda5cb4b23bd00479ac8d6538c873e17c0cf48b285a5c1"
+      url "https://github.com/dsimunic/elm-wrap/releases/download/v1.0.1/elm-wrap-macos-arm64"
+      sha256 "1c6baee0def03a458458d27eb7fdb2b1265bf252fd820b00e6177f434e935833"
     else
-      url "https://github.com/dsimunic/elm-wrap/releases/download/v1.0.0/elm-wrap-macos-amd64"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/dsimunic/elm-wrap/releases/download/v1.0.1/elm-wrap-macos-amd64"
+      sha256 "661366d09935771af46d4550f0e2d6aa0539f0b0ae9ef2c57fa14cfb34b2ce2b"
     end
   end
 
